5f60fe8f12fb53ec83d045a9f9986ca23b1dce96
anubhav anand <anubhav.ak22@gmail.com>
2022-12-05 23:04:33 +0530

    init

---
+++ app/src/main/java/com/ak22/android_drawer_navigation_tutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ak22.android_drawer_navigation_tutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
